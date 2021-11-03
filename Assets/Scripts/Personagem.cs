a9f9bd45c49e93f4e48a4d15e75613e87affa9f9
unknown <fernandonakagawa@outlook.com>
2021-11-03 17:09:24 -0300

    escada reformada e coleta de moedinhas

--- Assets/Scripts/Personagem.cs
+++ Assets/Scripts/Personagem.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Personagem : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public AudioClip SomPegarMoeda;
+    public GameObject TextoMoedas;
+
+    private int QtdMoedas;
     void Start()
     {
-        
+        this.QtdMoedas = 0;
+        AtualizarHUD();
     }
 
+    private void AtualizarHUD()
+    {
+        TextoMoedas.GetComponent<Text>().text = QtdMoedas.ToString();
+    }
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("coin"))
+        {
+            Destroy(col.gameObject);
+            this.GetComponent<AudioSource>().PlayOneShot(SomPegarMoeda);
+            this.QtdMoedas++;
+            AtualizarHUD();
+        }
     }
 }
